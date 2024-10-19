b59d0cfec9fb30d3e6a1191d334eaa4c4f3f4ce6
guilherme <guilherme.rdgo@outlook.com>
2024-10-18 21:15:54 -0300

    chore: seed correctly tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory(20)->create();
-        Project::factory(10)->create();
+        User::factory(10)->create();
+
+        User::query()->inRandomOrder()->limit(10)->get()->each(function ($user) {
+            Project::factory()->create(['created_by' => $user->id]);
+        });
+   
     }
 }
